6e02777bc8edce7acdfd91d49ce9d966e141efcd
sondos alansi <sondos.alansi@yahoo.com>
2018-09-17 18:56:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file
+function findMatching(drivers,name){
+  const drive=[];
+  for(const driver of drivers){
+    if(driver.toUpperCase()===name.toUpperCase()){
+     drive.push(driver);
+    }
+  }
+   return drive ;
+}
+
+function fuzzyMatch(drivers,letter){
+  const drive=[];
+   for(const driver of drivers){
+    if(driver.slice(0,letter.length)===letter){
+     drive.push(driver);
+    }
+  }
+   return drive ;
+  
+}
+
+function matchName(drivers,drivername){
+  const drive=[];
+  for(const driver of drivers){
+    if(driver.name===drivername){
+     drive.push(driver);
+    }
+  }
+   return drive ;
+  
+}
